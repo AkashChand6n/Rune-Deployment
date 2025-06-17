d2c0263e4064488da795abcefa436584422bf4a7
Akash Chandran <67275861+AkashChand6n@users.noreply.github.com>
2025-06-17 14:52:07 +0530

    Update database.py

--- Rune-backend/src/database.py
+++ Rune-backend/src/database.py
@@ -4,11 +4,10 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://rune_user:rune_pass@localhost/rune_db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
 
 def get_db():
